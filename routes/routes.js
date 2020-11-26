99fdc557f34c91f00a5e87bb74c3e530b8542e2a
Levente <Levente@DESKTOP-JPGKIQC>
2020-11-26 20:50:58 +0100

    2020.11.26.

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,6 @@ router.use('/work', require('./work'));
 router.use('/contact', require('./contact'));
 router.use('/messages', require('./messages'));
 router.use('/about', require('./about'));
-router.use('error', require('./error'));
+router.use('*', require('./error'));
 
 module.exports = router;
